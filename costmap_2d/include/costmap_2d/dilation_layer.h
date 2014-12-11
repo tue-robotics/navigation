d7f49025d4aa6344ff15cb8edcae0f0fcda91707
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2014-12-11 17:28:50 +0100

    Included plugin stuff in dilation_layer

--- costmap_2d/include/costmap_2d/dilation_layer.h
+++ costmap_2d/include/costmap_2d/dilation_layer.h
@@ -2,6 +2,8 @@
 #define COSTMAP_2D_DILATION_LAYER_H_
 
 #include "costmap_2d/layer.h"
+#include <dynamic_reconfigure/server.h>
+#include <costmap_2d/DilationPluginConfig.h>
 
 namespace costmap_2d
 {
@@ -17,16 +19,21 @@ public:
 
     void updateCosts(Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
 
+    virtual void onInitialize();
+
 private:
 
     unsigned char target_cell_value_, dilation_cell_value_;
 
-    // The dilation radius expressed in cells
-    int dilation_radius_cells_;
+    // The dilation radius expressed in meters
+    double dilation_radius_;
 
     // Squared distance map (unit = cells). Expresses the distance of a cell to the nearest border
     int* distance_sq_map_;
     unsigned int window_width_, window_height_;
+
+    dynamic_reconfigure::Server<costmap_2d::DilationPluginConfig> *dsrv_;
+    void reconfigureCB(costmap_2d::DilationPluginConfig &config, uint32_t level);
 
 };
 
