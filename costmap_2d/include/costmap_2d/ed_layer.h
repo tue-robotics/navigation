35578f0116ff14119430a2ee7f66c3597cbb7b5b
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2015-01-30 15:47:28 +0100

    Created ed layer plugin

--- costmap_2d/include/costmap_2d/ed_layer.h
+++ costmap_2d/include/costmap_2d/ed_layer.h
@@ -33,34 +33,63 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *
  * Author: Eitan Marder-Eppstein
+ *         David V. Lu!!
  *********************************************************************/
-#ifndef COSTMAP_COST_VALUES_H_
-#define COSTMAP_COST_VALUES_H_
-/** Provides a mapping for often used cost values */
+#ifndef ED_COSTMAP_PLUGIN_H_
+#define ED_COSTMAP_PLUGIN_H_
+#include <ros/ros.h>
+#include <costmap_2d/costmap_layer.h>
+#include <costmap_2d/layered_costmap.h>
+#include <costmap_2d/GenericPluginConfig.h>
+#include <dynamic_reconfigure/server.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <map_msgs/OccupancyGridUpdate.h>
+#include <message_filters/subscriber.h>
+
 namespace costmap_2d
 {
-/// No information
-static const unsigned char NO_INFORMATION = 255;
+class EdLayer : public CostmapLayer
+{
+public:
+  EdLayer();
+  virtual ~EdLayer();
+  virtual void onInitialize();
+  virtual void activate();
+  virtual void deactivate();
+  virtual void reset();
 
-/// Semantic annotations
-// Humans and moving obstacles (Range 250 - 254)
-static const unsigned char HUMAN = 254;
+  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x, double* min_y, double* max_x,
+                             double* max_y);
+  virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
 
-// Possibly moving objects (Range 240 - 249)
-static const unsigned char DOOR = 240;
+private:
+  /**
+   * @brief  Callback to update the costmap's map from the map_server
+   * @param new_map The map to put into the costmap. The origin of the new
+   * map along with its size will determine what parts of the costmap's
+   * static map are overwritten.
+   */
+  void incomingMap(const nav_msgs::OccupancyGridConstPtr& new_map);
+  void incomingUpdate(const map_msgs::OccupancyGridUpdateConstPtr& update);
+  void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
 
-// Small objects (Range 230 - 239)
-static const unsigned char GRASPABLE_OBJECT = 230;
+  unsigned char interpretValue(unsigned char value);
 
-// Large, possibly moving objects (furniture etc.) (Range 220 - 229)
-static const unsigned char UNKNONW_OBJECT = 225;
+  std::string global_frame_; ///< @brief The global frame for the costmap
+  bool subscribe_to_updates_;
+  bool map_received_;
+  bool has_updated_data_;
+  unsigned int x_,y_,width_,height_;
+  bool track_unknown_space_;
+  bool use_maximum_;
+  bool trinary_costmap_;
+  ros::Subscriber map_sub_, map_update_sub_;
 
-// Walls and stationary stuff (Range 202 - 209)
-static const unsigned char WALL = 202;
+  unsigned char lethal_threshold_, unknown_cost_value_;
 
-/// Lethal and lower values
-static const unsigned char LETHAL_OBSTACLE = 201;
-static const unsigned char INSCRIBED_INFLATED_OBSTACLE = 200;
-static const unsigned char FREE_SPACE = 0;
+  mutable boost::recursive_mutex lock_;
+  dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+};
 }
 #endif
+
